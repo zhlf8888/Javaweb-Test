9394d7ce2768d7e06b7ffde1e3808c50067426d3
zhlf8888 <904154180@qq.com>
2016-02-05 23:36:13 +0800

    nothing

--- src/com/bistu/javaweb/daoImpl/DataDAOImpl.java
+++ src/com/bistu/javaweb/daoImpl/DataDAOImpl.java
@@ -6,14 +6,6 @@ import java.sql.*;
 import com.bistu.javaweb.domain.Data;
 
 public interface DataDAOImpl {
-/*
-	public abstract Data creat(Data d) throws Exception;
-	public abstract int remove(Data d) throws Exception;
-	public abstract Data find(String d) throws Exception;
-	public abstract ResultSet findAll() throws Exception;
-	public abstract int update(Data d) throws Exception;
-}
-*/
 public abstract Data creat(Data d) throws Exception;
 public abstract int remove(Data d) throws Exception;
 public abstract Data find(String d) throws Exception;
